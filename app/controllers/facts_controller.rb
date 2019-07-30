57906000eb6d8744ae03be6a01e94cecf01169da
Madeline Young <madelinejy@gmail.com>
2019-07-30 22:19:52 +0000

    Finished Routes and created simple form page

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -1,5 +1,24 @@
 class FactsController < ApplicationController
 	def index
-		@fact = Fact.order("RANDOM()").first	
+		@fact = Fact.order("RANDOM()").first
+	end
+
+	def new
+		@fact = Fact.new
+	end
+
+	def show
+		@fact = Fact.find(params[:id])
+	end
+
+	def create
+		@fact = Fact.create(fact_params)
+		redirect_to @fact
+	end
+
+	private
+
+	def fact_params
+		params.require(:fact).permit(:content)
 	end
 end
